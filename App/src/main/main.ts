099b6e2576ea25b403c8328f5bcc0f953fb34300
Michael Lindner <Michael.Lindner@mth-it-service.com>
2025-07-18 21:51:38 +0200

    Check

--- App/src/main/main.ts
+++ App/src/main/main.ts
@@ -290,7 +290,7 @@ const createWindow = async () => {
 app.on('window-all-closed', () => {
   // Alle Shortcuts freigeben beim Schließen
   globalShortcut.unregisterAll();
-  
+
   // Respect the OSX convention of having the application in memory even
   // after all windows have been closed
   if (process.platform !== 'darwin') {
